05c2d397aa547aec36b3cd1cb236dc01caf79dca
Ingo <ingo.schaefer@adesso.de>
2024-11-04 16:45:51 +0100

    init commit

---
+++ tests/app.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("App splash page", async () => {
+  test('has title', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+
+  test('get started link', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
+
+  test('test', async ({ page }) => {
+    await page.goto('https://demo.playwright.dev/todomvc/#/');
+    await page.getByPlaceholder('What needs to be done?').click();
+    await page.getByPlaceholder('What needs to be done?').fill('eat lunch');
+    await page.getByPlaceholder('What needs to be done?').press('Enter');
+    await expect(page.locator('body')).toContainText('1 item left');
+    await page.getByLabel('Toggle Todo').check();
+    await page.getByPlaceholder('What needs to be done?').click();
+    await page.getByPlaceholder('What needs to be done?').fill('go to bed');
+    await page.getByPlaceholder('What needs to be done?').press('Enter');
+    await page.getByRole('link', { name: 'Completed' }).click();
+    await page.getByRole('link', { name: 'Active' }).click();
+    await expect(page.locator('body')).toContainText('1 item left');
+  });
+});
